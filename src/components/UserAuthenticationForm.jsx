e3a8dcca69a7a654b63f85cd81bfbed7fcbfe2cd
Sushant Kadam <sushantkadam15@gmail.com>
2023-11-10 21:27:01 -0500

    added Login opton

--- src/components/UserAuthenticationForm.jsx
+++ src/components/UserAuthenticationForm.jsx
@@ -9,27 +9,41 @@ const UserAuthenticationForm = () => {
   return (
     <Row className="h-screen">
       <Col xs={24} lg={14}>
-        <div className=" flex h-4/6 flex-col justify-center gap-20">
-          <div className=" mx-auto flex w-96 items-center gap-5">
-            <img src={logo} alt="Logo" /> <Title level={1}>Journiyfy</Title>
+        <Row className=" flex h-4/6 flex-col justify-center gap-20">
+          <div className=" mx-auto flex w-96 items-center justify-start gap-5">
+            <img src={logo} alt="Logo" />{" "}
+            <h1 className=" text-4xl">Journiyfy</h1>
           </div>
-          <div className=" mx-auto w-96">
-            <label className=" mb-1 block">Name</label>
-            <Input size="large" placeholder="Name" />
-            <label className=" mb-1 mt-4 block">Email</label>
-            <Input size="large" placeholder="Email" />
-            <label className=" mb-1 mt-4 block">Passowrd</label>
-            <Input size="large" placeholder="Password" />
-            <Button type="primary" className=" mt-8">Continue</Button>
+          <form className="mx-auto w-96 font-JetBrains">
+            <label htmlFor="name" className="mb-1 block">
+              Name
+            </label>
+            <Input id="name" size="middle" placeholder="Name" />
+
+            <label htmlFor="email" className="mb-1 mt-4 block">
+              Email
+            </label>
+            <Input id="email" size="middle" placeholder="Email" />
+
+            <label htmlFor="password" className="mb-1 mt-4 block">
+              Password
+            </label>
+            <Input id="password" size="middle" placeholder="Password" />
+
+            <Button htmlType="submit" type="primary" className="mt-8">
+              Sign Up
+            </Button>
+          </form>
+        </Row>
+        <Row className=" mt-10 h-[29%] bg-[#F9B3B30D]">
+          <div className="ml-10 mt-10 w-96 md:mx-auto">
+            <h2 className=" text-xl">Login</h2>
+            <p className=" mt-4 text-xs">Already have an account?</p>
+            <div className=" mt-5 flex gap-10">
+              <img src={googleIcon} alt="google logo" className="h-6 w-auto" />
+              <img src={appleIcon} alt="apple logo" className="h-6 w-auto" />
+            </div>
           </div>
-        </div>
-        <Row className="ml-10 mt-10 bg-[#F9B3B30D] md:ml-24">
-          <Col>
-            <Title level={4} className="">
-              Login
-            </Title>
-            <Paragraph>Already have an account?</Paragraph>
-          </Col>
         </Row>
       </Col>
       <Col xs={0} lg={10} className=" relative">
@@ -40,7 +54,7 @@ const UserAuthenticationForm = () => {
           muted
           className=" absolute h-full w-full object-cover"
         ></video>
-        <p className=" absolute right-[30%] mt-[30%] text-lg tracking-wider text-black">
+        <p className=" absolute mt-[30%] w-full text-center tracking-widest  text-black">
           A place to zen your thoughts.
         </p>
       </Col>
