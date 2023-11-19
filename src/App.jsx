786825197eb09c856ee09bdd8e8201edb1a9826b
Sushant Kadam <sushantkadam15@gmail.com>
2023-11-19 18:52:29 -0500

    Egitor.js configured

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,33 @@
-import UserAuthenticationForm from "./components/UserAuthenticationForm";
+import TextEditor from './components/TextEditor/TextEditor';
+import UserAuthenticationForm from './components/UserAuthenticationForm';
+import { useState } from 'react';
+
+// Initial Data
+const INITIAL_DATA = {
+    time: new Date().getTime(),
+    blocks: [
+        {
+            type: 'header',
+            data: {
+                text: 'This is my awesome editor!',
+                level: 1
+            }
+        }
+    ]
+};
 
 function App() {
-  return (
-    <section>
-      <UserAuthenticationForm />
-    </section>
-  );
+    const [data, setData] = useState(INITIAL_DATA);
+    return (
+        <section>
+            <TextEditor
+                data={data}
+                onChange={setData}
+                editorblock="editorjs-container"
+            />
+            {/* <UserAuthenticationForm /> */}
+        </section>
+    );
 }
 
 export default App;
