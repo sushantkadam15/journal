58d553a8a9ba8fe98f745edf322d773f3b8a2abc
Sushant Kadam <sushantkadam15@gmail.com>
2023-11-21 08:08:58 -0500

    Removed Editor JS

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,8 @@
-import TextEditor from './components/TextEditor/TextEditor';
 import UserAuthenticationForm from './components/UserAuthenticationForm';
 import { useState } from 'react';
 
-// Initial Data
-const INITIAL_DATA = {
-    time: new Date().getTime(),
-    blocks: [
-        {
-            type: 'header',
-            data: {
-                text: 'This is my awesome editor!',
-                level: 1
-            }
-        }
-    ]
-};
-
 function App() {
-    const [data, setData] = useState(INITIAL_DATA);
-    return (
-        <section>
-            <TextEditor
-                data={data}
-                onChange={setData}
-                editorblock="editorjs-container"
-            />
-            {/* <UserAuthenticationForm /> */}
-        </section>
-    );
+    return <section>{/* <UserAuthenticationForm /> */}</section>;
 }
 
 export default App;
